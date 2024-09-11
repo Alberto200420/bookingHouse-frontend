e7f3918ca8bcd59a48597bd3de141a147e5038a6
Alberto200420 <105571383+Alberto200420@users.noreply.github.com>
2024-09-11 16:26:43 -0600

    Initail config

---
+++ app/(users)/[hotel]/page.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Hoteles",
+  description: "Disfruta de tus vacaciones, reserva servicios, encuentra todo lo que necesitas para ti.",
+};
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        Hola desde hotel path []
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
